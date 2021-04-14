64df958946cbcd2e5b8bdf8c6cf42485ff55d451
Ade Yusuf Yaumul Isnain <yaumulisnain@gmail.com>
2021-04-14 12:09:24 +0700

    + Update .gitignore; + Setup redis

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module simple-redis-cli
 
 go 1.15
+
+require (
+	github.com/gomodule/redigo v1.8.4
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"simple-redis-cli/src"
 )
 
 func main() {
 	fmt.Println("redis-simple-cli")
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		os.Exit(1)
+	}
+
+	src.InitRedis()
 }
